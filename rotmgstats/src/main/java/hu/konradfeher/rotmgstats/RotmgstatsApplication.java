71ee95a047445da83fcd5ea1b55aec7ffac577b1
KonradFeher <konradfeher@outlook.com>
2022-12-04 11:55:57 +0100

    spring initializr

---
+++ rotmgstats/src/main/java/hu/konradfeher/rotmgstats/RotmgstatsApplication.java
@@ -0,0 +1,13 @@
+package hu.konradfeher.rotmgstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotmgstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotmgstatsApplication.class, args);
+	}
+
+}
---
+++ rotmgstats/src/test/java/hu/konradfeher/rotmgstats/RotmgstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.konradfeher.rotmgstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotmgstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
